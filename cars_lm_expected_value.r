1ab3697d976f2d861e4ff8d726257a017511774c
KoretsuNobuyasu <einsatzkommandocobra.1996@gmail.com>
2018-10-21 00:14:52 +0900

    修正

--- cars_lm_expected_value.r
+++ cars_lm_expected_value.r
@@ -1,10 +1,8 @@
+cars
 cars.lm <- lm(dist ~ speed, data=cars)
 cars.lm
 
-cars.lm <- lm(y~x, data=sample)
-print(cars.lm)
-
-b <- cov(sample$x, sample$y) / var(sample$x)
-a <- mean(sample$y) - b*mean(sample$x)
+b<-cov(cars$speed, cars$dist) / var(cars$speed)
+a<-mean(cars$dist) - b*mean(cars$speed)
 a
 b
